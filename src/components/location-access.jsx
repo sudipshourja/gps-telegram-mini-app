7ac470adf9c63cae0e1ade0ca732e6f96d4dd86b
Ojoibo Podartho <sudipshourja@Sudips-MacBook-Air.local>
2025-03-12 00:32:13 +0600

    New Bot

--- src/components/location-access.jsx
+++ src/components/location-access.jsx
@@ -15,26 +15,19 @@ const LocationAccess = ({ setLocationData }) => {
         console.log("Failed to initialize LocationManager.");
       }
     });
-    window.Telegram.WebApp.LocationManager.getLocation((locationData) => {
-      if (locationData) {
-        setLocationData(locationData);
-        // console.log("Location data received:", locationData);
-        // console.log("Latitude:", locationData.latitude);
-        // console.log("Longitude:", locationData.longitude);
-        // console.log("Accuracy:", locationData.accuracy, "meters");
-      } else {
-        console.log("Access to location was not granted.");
-      }
-    });
-
-    window.Telegram.WebApp.LocationManager.getLocation((locationData) => {
-      if (!locationData) {
-        console.warn("User denied access to location data.");
-        alert("Location access is required for this feature.");
-      } else {
-        console.log("Location data:", locationData);
-      }
-    });
+    const interval = setInterval(async () => {
+      window.Telegram.WebApp.LocationManager.getLocation((locationData) => {
+        if (locationData) {
+          // setLocationData(locationData);
+          // console.log("Location data received:", locationData);
+          // console.log("Latitude:", locationData.latitude);
+          // console.log("Longitude:", locationData.longitude);
+          // console.log("Accuracy:", locationData.accuracy, "meters");
+        } else {
+          console.log("Access to location was not granted.");
+        }
+      });
+    }, 5000);
   };
 
   return (
